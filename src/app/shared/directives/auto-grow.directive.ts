b5fd40b9b3e76fe6647c5e41e72cd112af9fc3d0
sagarsys <sagar_sys@live.com>
2018-03-04 22:32:02 +0400

    - HostBinding

--- src/app/shared/directives/auto-grow.directive.ts
+++ src/app/shared/directives/auto-grow.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appInputAutoGrow]',
 })
 
 export class AutoGrowDirective {
-
+  
+  @HostBinding('class.hover') private isHovering = false;
+  
   constructor(private el: ElementRef, private render: Renderer2) {}
   
   @HostListener('focus')
@@ -16,6 +18,16 @@ export class AutoGrowDirective {
   @HostListener('blur')
   onBlur() {
     this.render.setStyle(this.el.nativeElement, 'width', '125px');
+  }
+  
+  @HostListener('mouseover')
+  onMouseOver() {
+    this.isHovering = true;
+  }
+  
+  @HostListener('mouseout')
+  onMouseOut() {
+    this.isHovering = false;
   }
   
 }
